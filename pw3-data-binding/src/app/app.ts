89ce50adfbb0d32b6fe8f9c257285ad5e60075c4
Aalvebia <aalvebia03@gmail.com>
2026-03-31 10:51:15 -0300

    Projeto Biscoito da Sorte

--- pw3-data-binding/src/app/app.ts
+++ pw3-data-binding/src/app/app.ts
@@ -14,9 +14,7 @@ export class App {
     "O bom-senso vale mais do que muito conhecimento.",
     "O riso é a menor distância entre duas pessoas.",
     "Deixe de lado as preocupações e seja feliz.",
-    "Realize o óbvio, pense no improvável e conquiate o impossível.",
-    "Acredite em milagres, mas não dependa deles.",
-    "A maior barreira para o sucesso é o fracasso.",
+    "Realize o óbvio, pense no improvável e conquiate o impossível."
   ]
 
   mensagem = "";
@@ -27,8 +25,6 @@ export class App {
     if(this.fechado){
       this.fechado = false;
       this.img_biscoito = "quebrado.png";
-      let indice =  Math.floor(Math.random() * this.frases.length);
-      this.mensagem = this.frases[indice];
     }
   }
 
